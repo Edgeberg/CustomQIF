15cb335094a3c823f758a2654a29b2606c574c14
Edgeberg_cp <SND\Edgeberg_cp>
2016-03-08 05:58:28 +0000

    Import my CustomQIF NetBeans project; 2009-11-03 22:26:35 +1100

---
+++ trunk/CustomQIF/src/customqif/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on 19 March 2007, 23:10
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package customqif;
+
+/**
+ *
+ * @author eldon_r
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frmCustomiseQIF().setVisible(true);
+    }
+    
+}
