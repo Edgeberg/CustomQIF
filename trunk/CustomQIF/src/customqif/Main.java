abff2dbd55047ab37447e9de1cff3d427c1eb6d2
Edgeberg_cp <SND\Edgeberg_cp>
2016-03-09 00:24:58 +0000

    Add general documentation (project purpose, general notes); 2009-12-02 22:22:24 +1100

--- trunk/CustomQIF/src/customqif/Main.java
+++ trunk/CustomQIF/src/customqif/Main.java
@@ -3,8 +3,28 @@
  *
  * Created on 19 March 2007, 23:10
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * This program was created to store and use regular expression strings for
+ * matching transaction in Quicken format transaction import files (.QIF
+ * extension) to accounts in an accounting package (GnuCash) that I wanted to
+ * import these transactions into.
+ *
+ * This process greatly simplifies the import process if the aim is to split all
+ * transactions into meaningful categories (represented by different accounts)
+ * but the source .QIF file is a simplified one from a single account at a bank.
+ *
+ * I also found, once I had switched banks, that my new bank's export files
+ * have multi-line transaction narrations that are not gathered into a single
+ * transaction in the file.  So I added logic to gather these narrations onto
+ * the narration line of the transaction to which they belong.  There was one
+ * tricky exception to the rule of "multi-line transaction narrations occur
+ * sequentially after the parent transaction without being interrupted by other
+ * transaction records" -- and that was for the mortgage interest transactions,
+ * where so far one (1) transaction/narration group is interrupted by an
+ * unrelated transaction that occurred on the same date.  So I also added logic
+ * to recognise these types of transactions and to search for the other part
+ * past other transactions if necessary.  I deemed it wouldn't accidentally (all
+ * things being normal) come across the next such transaction & try to
+ * incorporate it, since these only occur monthly.
  */
 
 package customqif;
