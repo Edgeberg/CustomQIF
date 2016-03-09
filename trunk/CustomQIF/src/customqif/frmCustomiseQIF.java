c36c630981c9ae31c937b0900e9518edde98a9bd
Edgeberg_cp <SND\Edgeberg_cp>
2016-03-09 00:11:27 +0000

    Remove an unused import; 2009-12-02 21:56:26 +1100

--- trunk/CustomQIF/src/customqif/frmCustomiseQIF.java
+++ trunk/CustomQIF/src/customqif/frmCustomiseQIF.java
@@ -12,7 +12,6 @@ import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
